25f913cd7230fb0018d560f0f2c6e042d856b9b2
jem <jeremyxarrabe@gmail.com>
2025-12-01 23:52:31 +0800

    Initial Commit

---
+++ youtube-music-player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  youtube-music-player
+//
+//  Created by Jem on 12/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ youtube-music-player/youtube_music_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_music_playerApp.swift
+//  youtube-music-player
+//
+//  Created by Jem on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct youtube_music_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
